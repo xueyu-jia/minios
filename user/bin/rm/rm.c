4800e92fe0292e7a8d30ea53ff8fb876fbd5aec5
周上行Ryer <melaiiryer@gmail.com>
2025-02-22 17:36:07 +0800

    refactor: rm

--- user/bin/rm/rm.c
+++ user/bin/rm/rm.c
@@ -4,53 +4,51 @@
 #include <string.h>
 #include <sys/stat.h>
 
-struct stat statbuf;
-int remove(const char* path, int rec) {
-    if (stat(path, &statbuf) == 0) {
-        if (statbuf.st_type == I_REGULAR) {
-            return unlink(path);
-        } else if (statbuf.st_type == I_DIRECTORY) {
-            if (rec == 1) {
-                char* _path = (char*)malloc(MAX_PATH);
-                strcpy(_path, path);
-                if (_path[strlen(_path) - 1] != '/') { strcat(_path, "/"); }
-                int len = strlen(_path);
+int remove(const char* path, bool recursive) {
+    struct stat statbuf = {};
+    const int retval = stat(path, &statbuf);
+    if (retval != 0) { return -1; }
+    switch (statbuf.st_type) {
+        case I_DIRECTORY: {
+            if (recursive) {
+                char* pathbuf = malloc(MAX_PATH);
+                strcpy(pathbuf, path);
+                if (pathbuf[strlen(pathbuf) - 1] != '/') { strcat(pathbuf, "/"); }
+                int len = strlen(pathbuf);
                 DIR* dirp = opendir(path);
                 struct dirent* sub;
                 while ((sub = readdir(dirp)) != NULL) {
                     if ((strcmp(sub->d_name, ".") == 0) || (strcmp(sub->d_name, "..") == 0)) {
                         continue;
                     }
-                    strcat(_path, sub->d_name);
-                    // printf("%s %d ", _path, rec);
-                    remove(_path, rec);
-                    _path[len] = 0;
+                    strcat(pathbuf, sub->d_name);
+                    remove(pathbuf, recursive);
+                    pathbuf[len] = 0;
                 }
                 closedir(dirp);
-                free(_path);
+                free(pathbuf);
             }
             return rmdir(path);
-        }
+        } break;
+        case I_REGULAR: {
+            return unlink(path);
+        } break;
     }
     return -1;
 }
 
 int main(int argc, char* argv[]) {
-    int recursicve = 0, opt;
+    bool recursicve = false;
+    char opt = '\0';
     while ((opt = getopt(argc, argv, "r")) != -1) {
-        switch (opt) {
-            case 'r':
-                recursicve = 1;
-                break;
-
-            default:
-                break;
+        if (false) {
+        } else if (opt == 'r') {
+            recursicve = true;
+        } else {
+            break;
         }
     }
-    if (optind < argc) {
-        return remove(argv[optind], recursicve);
-    } else {
-        fprintf(STD_ERR, "no target to remove\n");
-        return -1;
-    }
+    if (optind < argc) { return remove(argv[optind], recursicve); }
+    fprintf(stderr, "no target to remove\n");
+    return -1;
 }
