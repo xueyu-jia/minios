86beadc225c5f220e904fb385d2e2465297b700a
周上行Ryer <melaiiryer@gmail.com>
2025-03-02 17:11:31 +0800

    fix: uart lsr check

--- arch/x86/boot/mbr/serial.c
+++ arch/x86/boot/mbr/serial.c
@@ -3,28 +3,6 @@
 
 #define PORT_COM1 0x3f8
 
-bool serial_init() {
-    outb(PORT_COM1 + 1, 0x00); //<! disable all interrupts
-    outb(PORT_COM1 + 3, 0x80); //<! enable dlab (set baud rate divisor)
-    outb(PORT_COM1 + 0, 0x03); //<! set divisor to 3 (lo byte) 38400 baud
-    outb(PORT_COM1 + 1, 0x00); //<! (hi byte)
-    outb(PORT_COM1 + 3, 0x03); //<! 8 bits, no parity, one stop bit
-    outb(PORT_COM1 + 2, 0xc7); //<! enable fifo, clear them, with 14-byte threshold
-    outb(PORT_COM1 + 4, 0x0b); //<! irqs enabled, rts/dsr set
-    outb(PORT_COM1 + 4, 0x1e); //<! set in loopback mode, test the serial chip
-
-    //! test the serial chip by sending a byte and checking if it is received correctly
-    const uint8_t test_byte = 0xae;
-    outb(PORT_COM1, test_byte);
-    if (inb(PORT_COM1) != test_byte) { return false; }
-
-    //! if serial is not faulty set it in normal operation mode, i.e. not-loopback with IRQs enabled
-    //! and OUT#1 and OUT#2 bits enabled
-    outb(PORT_COM1 + 4, 0x0f);
-
-    return true;
-}
-
 static inline int is_transmit_empty() {
     return inb(PORT_COM1 + 5) & 0x20;
 }
@@ -41,4 +19,27 @@ uint8_t serial_read() {
 void serial_write(uint8_t byte) {
     while (!is_transmit_empty()) {}
     outb(PORT_COM1, byte);
+}
+
+bool serial_init() {
+    outb(PORT_COM1 + 1, 0x00); //<! disable all interrupts
+    outb(PORT_COM1 + 3, 0x80); //<! enable dlab (set baud rate divisor)
+    outb(PORT_COM1 + 0, 0x03); //<! set divisor to 3 (lo byte) 38400 baud
+    outb(PORT_COM1 + 1, 0x00); //<! (hi byte)
+    outb(PORT_COM1 + 3, 0x03); //<! 8 bits, no parity, one stop bit
+    outb(PORT_COM1 + 2, 0xc7); //<! enable fifo, clear them, with 14-byte threshold
+    outb(PORT_COM1 + 4, 0x0b); //<! irqs enabled, rts/dsr set
+    outb(PORT_COM1 + 4, 0x1e); //<! set in loopback mode, test the serial chip
+
+    //! test the serial chip by sending a byte and checking if it is received correctly
+    const uint8_t test_byte = 0xae;
+    serial_write(test_byte);
+    const uint8_t received_byte = serial_read();
+    if (received_byte != test_byte) { return false; }
+
+    //! if serial is not faulty set it in normal operation mode, i.e. not-loopback with IRQs enabled
+    //! and OUT#1 and OUT#2 bits enabled
+    outb(PORT_COM1 + 4, 0x0f);
+
+    return true;
 }
--- kernel/uart.c
+++ kernel/uart.c
@@ -1,45 +1,43 @@
 #include <minios/uart.h>
 #include <minios/asm.h>
 
-int init_simple_serial() {
-    outb(PORT_COM1 + 1, 0x00); // Disable all interrupts
-    outb(PORT_COM1 + 3, 0x80); // Enable DLAB (set baud rate divisor)
-    outb(PORT_COM1 + 0, 0x03); // Set divisor to 3 (lo byte) 38400 baud
-    outb(PORT_COM1 + 1, 0x00); // (hi byte)
-    outb(PORT_COM1 + 3, 0x03); // 8 bits, no parity, one stop bit
-    outb(PORT_COM1 + 2,
-         0xc7);                // Enable FIFO, clear them, with 14-byte threshold
-    outb(PORT_COM1 + 4, 0x0b); // IRQs enabled, RTS/DSR set
-    outb(PORT_COM1 + 4, 0x1e); // Set in loopback mode, test the serial chip
-    outb(PORT_COM1 + 0, 0xae); // Test serial chip (send byte 0xAE and check if
-                               // serial returns same byte)
-
-    // Check if serial is faulty (i.e: not same byte as sent)
-    if (inb(PORT_COM1 + 0) != 0xae) {
-        // panic("faulty serial");
-        return 1;
-    }
-
-    // If serial is not faulty set it in normal operation mode
-    // (not-loopback with IRQs enabled and OUT#1 and OUT#2 bits enabled)
-    outb(PORT_COM1 + 4, 0x0f);
-    return 0;
-}
-
 static inline int is_transmit_empty() {
     return inb(PORT_COM1 + 5) & 0x20;
 }
 
 static inline int serial_received() {
-    return inb(PORT_COM1 + 5) & 1;
+    return inb(PORT_COM1 + 5) & 0x01;
 }
 
 char serial_read() {
-    while (serial_received() == 0) {}
+    while (!serial_received()) {}
     return inb(PORT_COM1);
 }
 
-void serial_write(char a) {
-    while (is_transmit_empty() == 0) {}
-    outb(PORT_COM1, a);
+void serial_write(char byte) {
+    while (!is_transmit_empty()) {}
+    outb(PORT_COM1, byte);
+}
+
+int init_simple_serial() {
+    outb(PORT_COM1 + 1, 0x00); //<! disable all interrupts
+    outb(PORT_COM1 + 3, 0x80); //<! enable dlab (set baud rate divisor)
+    outb(PORT_COM1 + 0, 0x03); //<! set divisor to 3 (lo byte) 38400 baud
+    outb(PORT_COM1 + 1, 0x00); //<! (hi byte)
+    outb(PORT_COM1 + 3, 0x03); //<! 8 bits, no parity, one stop bit
+    outb(PORT_COM1 + 2, 0xc7); //<! enable fifo, clear them, with 14-byte threshold
+    outb(PORT_COM1 + 4, 0x0b); //<! irqs enabled, rts/dsr set
+    outb(PORT_COM1 + 4, 0x1e); //<! set in loopback mode, test the serial chip
+
+    //! test the serial chip by sending a byte and checking if it is received correctly
+    const uint8_t test_byte = 0xae;
+    serial_write(test_byte);
+    const uint8_t received_byte = serial_read();
+    if (received_byte != test_byte) { return -1; }
+
+    //! if serial is not faulty set it in normal operation mode, i.e. not-loopback with IRQs enabled
+    //! and OUT#1 and OUT#2 bits enabled
+    outb(PORT_COM1 + 4, 0x0f);
+
+    return 0;
 }
